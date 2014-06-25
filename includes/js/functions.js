5312c987aff9603ec71dffe9eca24f8eec334577
JMarqz <jmarqz04@gmail.com>
2014-06-25 16:06:31 -0500

    Directorio ASD
    Cordova 3.5.0
    Modificación de los íconos.
    CSS actualizado.
    Función para abrir Twitter.
    Modificación de "Créditos" y "Asociaciones".

--- includes/js/functions.js
+++ includes/js/functions.js
@@ -1,10 +1,36 @@
-// FUNCIONES A UTILIZAR
 
+//Abrir URL en navegador nativo
 function abrirURL(url) {
-    // Esperar a que Cordova cargue.
-    document.addEventListener('deviceready', onDeviceReady, false);
-    // Cordova está listo
+    document.addEventListener('deviceready', onDeviceReady, false); // Esperar a que Cordova cargue.
+    
     function onDeviceReady() {
-        var ref = window.open(encodeURI(url), '_system', 'closebuttoncaption=Cerrar');
+        var ref = window.open(encodeURI(url), '_system');
     }
+}
+
+//Abrir Twitter
+function abrirTwitter(){
+	document.addEventListener("deviceready", onDeviceReady, false);
+
+    function onDeviceReady() {
+        var plataforma = device.platform;
+        var twitter;
+
+        //Saber si es Android o iOS
+        if (plataforma == "Android") {
+            twitter = "com.twitter.android";
+        } else if(plataforma == "iOS"){
+            twitter = "twitter://";
+        };
+
+        //Checar Twitter v0.2.1
+        appAvailability.check(twitter, function(availability) {
+            // availability is either true or false
+            if(availability) {
+                window.open("twitter://user?screen_name="+userName);
+            } else{
+                window.open("http://www.twitter.com/"+userName, "_system");
+            }
+        });
+    }  
 }
