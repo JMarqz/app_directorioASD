da1d88d4107289718f2edcad787662f991ef8f87
JMarqz <jmarqz04@gmail.com>
2014-07-28 15:10:08 -0500

    Directorio Adventista
    Modificación de datos y optimización del código.

--- includes/js/functions.js
+++ includes/js/functions.js
@@ -10,37 +10,29 @@ function onDeviceReady() {
 
 //Abrir URL en navegador nativo
 function abrirURL(url) {
-    //document.addEventListener('deviceready', onDeviceReady, false); // Esperar a que Cordova cargue.
-    
-    //function onDeviceReady() {
-        var ref = window.open(encodeURI(url), '_system');
-    //}
+    var ref = window.open(encodeURI(url), '_system');
 }
 
 
 //Abrir Twitter
 function abrirTwitter(userName){
-	//document.addEventListener("deviceready", onDeviceReady, false);
+    var plataforma = device.platform;
+    var twitter;
 
-    //function onDeviceReady() {
-        var plataforma = device.platform;
-        var twitter;
+    //Saber si es Android o iOS
+    if (plataforma == "Android") {
+        twitter = "com.twitter.android";
+    } else if(plataforma == "iOS"){
+        twitter = "twitter://";
+    };
 
-        //Saber si es Android o iOS
-        if (plataforma == "Android") {
-            twitter = "com.twitter.android";
-        } else if(plataforma == "iOS"){
-            twitter = "twitter://";
-        };
-
-        // AppAvailability v0.3.0
-        appAvailability.check(
-        twitter, // URI Scheme
-        function() {  // Success callback
-            window.open("twitter://user?screen_name="+userName);
-        },
-        function() {  // Error callback
-            window.open("http://www.twitter.com/"+userName, "_system");
-        });
-    //}
+    // AppAvailability v0.3.0
+    appAvailability.check(
+    twitter, // URI Scheme
+    function() {  // Success callback
+        window.open("twitter://user?screen_name="+userName);
+    },
+    function() {  // Error callback
+        window.open("http://www.twitter.com/"+userName, "_system");
+    });
 }
